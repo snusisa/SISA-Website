a7e267868bc5f683b9e9db35dbef619dd9c220b4
Rachel Gan <ganrachel98@gmail.com>
2020-02-29 20:06:40 +0900

    Add meta charset

--- CampusLife/TabComponents/studentSupportPrograms.js
+++ CampusLife/TabComponents/studentSupportPrograms.js
@@ -1,7 +1,13 @@
 document.write(`<!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Student Support Programs</title>
+        <meta charset="UTF-8" />
+        <meta
+            name="viewport"
+            content="width=<device-width>, initial-scale=1.0"
+        />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />    
+    <title>Student Support Programs</title>
     </head>
     <div class="table-body">
         <p>
